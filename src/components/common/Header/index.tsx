2f475f0f49e5c823cec8caef0b9a321e1bddac3c
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2023-02-19 22:14:02 +0200

    build the header component

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+const Header: React.FC = () => {
+    return (
+        <div className='bk'>
+            <section className="header-section">
+
+                  <p>   
+                     <i className="fas fa-duotone fa-shop"></i>
+                    POS store</p>
+            </section ></div>
+    )
+}
+
+export default Header
