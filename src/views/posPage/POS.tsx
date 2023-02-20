0c8fd110abebb84703c02c3d96fa465b3cf648e6
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2023-02-20 20:22:52 +0200

    complete pos page and handle search

--- src/views/posPage/POS.tsx
+++ src/views/posPage/POS.tsx
@@ -1,21 +1,25 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import Category from '../../components/CategoryCart'
 import ProductCard from '../../components/productCard'
 import './style.scss'
 import { category, products } from '../../types'
 import Cart from './Cart/Cart'
 import Header from '../../components/common/Header'
-
-import { productsContext } from '../../context/productContext'
 import { categoriesContext } from '../../context/CategoryContext'
-import Category from '../../components/CategoryCart'
-export const POS: React.FC = () => {
-    const sampleProductsContext = useContext(productsContext)
+import { productsContext } from '../../context/productContext'
+interface props {
+
+}
+export const POS: React.FC<props> = () => {
     const sampleAppContext = useContext(categoriesContext);
+    const sampleProductsContext = useContext(productsContext)
     const [activeItem, setActiveItem] = useState<category | null>(null);
     const [isAllActive, setIsAllActive] = useState<boolean>(true);
     const [products, setProducts] = useState<products[] | undefined>(sampleProductsContext?.products)
+    const [input, setInput] = useState('')
     const handleClick = (category: category | undefined) => {
         setActiveItem(category || null);
+        setInput('')
         setIsAllActive(false);
         console.log(category, "category")
         const c = category?.category?.toLocaleLowerCase()
@@ -24,12 +28,41 @@ export const POS: React.FC = () => {
         ))
     };
     const handleAllClick = () => {
+        setInput('')
         setActiveItem(null);
         setIsAllActive(true);
         setProducts(sampleProductsContext?.products)
     };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value)
 
+        const searchTerm = e.target.value.trim()
+        console.log(searchTerm)
+        if (searchTerm !== '')
+        {
+            setProducts(products?.filter((product) => {
+                const productName = product.title.toLowerCase();
+                if (!isAllActive)
+                {
+                    return productName.includes(
+                        searchTerm.toLowerCase())
+                        && activeItem?.category?.toLowerCase() === product.category?.toLowerCase();
+                }
+
+                return productName.includes(searchTerm.toLowerCase())
+            }))
+        }
+        else
+        {
+            !isAllActive ? setProducts(sampleProductsContext?.products?.
+                filter(product => { return product?.category?.toLowerCase() === activeItem?.category?.toLowerCase() }))
+                : setProducts(sampleProductsContext?.products)
+
+        }
+
+    }
     return (
+
         <>
             <main className="main-section">
                 <Header />
@@ -45,7 +78,7 @@ export const POS: React.FC = () => {
                     }
                 </section>
                 <div className='search' >
-                    <input type="text" placeholder='search...' />
+                    <input type="text" placeholder='search...' value={input} onChange={(e) => handleChange(e)} />
                 </div>
                 <section className="product-section">
 
