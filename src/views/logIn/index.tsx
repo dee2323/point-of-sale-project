51af58be3dea668cc49dd9f9ffe48674a5343561
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2023-02-20 22:24:13 +0200

    refactor login with formik

--- src/views/logIn/index.tsx
+++ src/views/logIn/index.tsx
@@ -1,68 +1,64 @@
-
-import React, { useState, ChangeEvent, SyntheticEvent } from "react"
-import Button from "../../components/common/Button"
+import React from "react";
+import Button from "../../components/common/Button";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../config/firebase/firebase"
+import { auth } from "../../config/firebase/firebase";
 import { useNavigate } from "react-router-dom";
-import { useFormik } from 'formik'
-import './style.scss'
-const initState = {
-    email: '',
-    password: ''
-}
+import { useFormik } from "formik";
+import "./style.scss";
+
 const Login: React.FC = () => {
-    const [input, setInput] = useState(initState)
-    const [error, setError] = useState<Error | null>(null)
-    const handleSubmit = async (e: SyntheticEvent) => {
-        e.preventDefault();
-        try
-        {
-            await signInWithEmailAndPassword(auth, input.email, input.password)
-            setInput(initState)
+    const navigate = useNavigate();
 
-        } catch (e)
-        {
-            if (e instanceof Error)
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: async (values, { setSubmitting, setFieldError }) => {
+            try
             {
-                setError(e);
-                console.log(e.message)
+                await signInWithEmailAndPassword(auth, values.email, values.password);
+                navigate("/");
+            } catch (error)
+            {
+                setFieldError("password", "Invalid email or password");
             }
-        }
-
-
-
-    }
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
-        setError(null)
-    }
-    
+            setSubmitting(false);
+        },
+    });
 
     return (
-        <div className='background'>
+        <div className="background">
             <div className="login-container">
-
                 <div className="left-side">
                     <img src="./images/shop.jpg" />
                     <h2>Get started</h2>
                     <p>use for your sales bussnise</p>
                 </div>
-                <div className='right-side'>
-                    <form onSubmit={(e) => handleSubmit(e)}>
+                <div className="right-side">
+                    <form onSubmit={formik.handleSubmit}>
                         <h2>Get started</h2>
                         <p>please login with your Email and use for your sales bussnise</p>
-                        <input type="text" placeholder='Email' onChange={(e) => handleChange(e)} name='email' />
-                        <input type="password" placeholder='password' onChange={(e) => handleChange(e)} name='password' />
-
-                        <Button text='login' onClick={() => { }} />
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            {...formik.getFieldProps("email")}
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                            <p className="erro">{formik.errors.email}</p>
+                        ) : null}
+                        <input
+                            type="password"
+                            placeholder="password"
+                            {...formik.getFieldProps("password")}
+                        />
+                        <Button text="login" onClick={() => { }} />
                     </form>
+                    <p className="error">{formik.errors.password}</p>
                 </div>
-                {error && <p>{error?.message}</p>}
-            </div></div>
-    )
-}
+            </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
