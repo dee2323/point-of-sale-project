66f002a15b8545a9c45f339dd6e3263c71a9dab4
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2023-02-21 09:44:01 +0200

    add,delete,update product in the product context

--- src/context/productContext.tsx
+++ src/context/productContext.tsx
@@ -4,6 +4,9 @@ import { products } from "../types";
 interface valueInterface {
   products: products[],
   loading: boolean,
+  handleAddingProduct: (product: products) => void,
+  handleDeletingproduct?: (id: string) => void;
+  handleUpdateProduct: (id: string, product: products) => void,
 }
 export const productsContext = createContext<valueInterface | null>(null);
 interface props {
@@ -20,14 +23,14 @@ const ProductContextProvider: React.FC<props> = ({ children }: props) => {
     try
     {
       const response = await axios.get("https://pos-project-deema-default-rtdb.firebaseio.com/products/products.json/");
-      
+
 
       if (response.data !== null)
       {
         const result = Object.keys(response.data).map((key) => {
-          
+
           const product = response.data[key];
-          
+
 
           if (product !== null)
           {
@@ -37,7 +40,7 @@ const ProductContextProvider: React.FC<props> = ({ children }: props) => {
         });
 
         setProducts(result.filter((product) => product !== undefined));
-        
+
       } else
       {
         setProducts([]);
@@ -52,14 +55,31 @@ const ProductContextProvider: React.FC<props> = ({ children }: props) => {
   useEffect(() => {
     getProducts();
   }, []);
+  const handleAddingProduct = async (product: products) => {
+    await axios.post("https://pos-project-deema-default-rtdb.firebaseio.com/products/products.json",
+      { ...product, id: products.length });
+    getProducts();
+  };
 
+  const handleDeletingproduct = async (id: string) => {
+    await axios.delete("https://pos-project-deema-default-rtdb.firebaseio.com/products/products/" + id + ".json");
+    getProducts();
+  };
 
+  const handleUpdateProduct = async (id: string, product: products) => {
+    await axios.patch(
+      "https://pos-project-deema-default-rtdb.firebaseio.com/products/products/" + id + ".json",
+      { ...product }
+    );
+    getProducts();
+  };
 
   const sampleAppContext: valueInterface = {
-
+    handleAddingProduct,
     products,
     loading,
-
+    handleDeletingproduct,
+    handleUpdateProduct,
   };
 
   return <productsContext.Provider value={sampleAppContext}>{children}</productsContext.Provider>
