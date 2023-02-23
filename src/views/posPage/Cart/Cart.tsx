56ad4ab3b5db6599d2dab70b80b8bb716d72e8d9
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2023-02-23 18:30:30 +0200

    build the functionality to cart section

--- src/views/posPage/Cart/Cart.tsx
+++ src/views/posPage/Cart/Cart.tsx
@@ -1,35 +1,105 @@
-import React from 'react'
-import Button from '../../../components/common/Button'
-import './style.scss'
+import React, { useContext, useState } from 'react';
+import Button from '../../../components/common/Button';
+import { cartContext } from '../../../context/CartContext';
+import './style.scss';
 
 const Cart: React.FC = () => {
+    const sampleCartContext = useContext(cartContext);
+    const [input, setInput] = useState({
+        tax: 0,
+        discount: 0,
+    });
+
+    const subTotalPrice = sampleCartContext?.cartProducts.reduce((total, num) => {
+        return total + num.count * num.price;
+    }, 0) || 0;
+
+    const tax = input.tax / 100;
+    const discount = input.discount / 100;
+    const total = subTotalPrice * (1 + tax) * (1 - discount);
+
+    const handleTaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput({ ...input, tax: parseFloat(e.target.value) });
+    };
+
+    const handleDiscountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput({ ...input, discount: parseFloat(e.target.value) });
+    };
+
     return (
         <section className="cart-section">
-            <div className='header'>
+            <div className="header">
                 <h3>custormers order</h3>
-
             </div>
-            <div className='items-section'>
+            <div className="items-section">
                 <ul>
-                    <li className="item"><span className="item-image">
-                        <img src='./images/shopImage.jpg' height='45rem' width='45rem' /> </span><span className="head">{'title'}</span><span className="item-price">${'price'}</span>
-                        <i className="fas fa-regular fa-trash" />
-                    </li >
-                    <li className="item"><span className="item-image">
-                        <img src='./images/shop.jpg' height='45rem' width='45rem' /> </span><span className="head">{'title'}</span><span className="item-price">${'price'}</span>
-                        <i className="fas fa-regular fa-trash" />
-                    </li >
-                    <li className="item"><span className="item-image">
-                        <img src='./images/shop.jpg' height='45rem' width='45rem' /> </span><span className="head">{'title'}</span><span className="item-price">${'price'}</span>
-                        <i className="fas fa-regular fa-trash" />
-                    </li >
-
-
+                    {sampleCartContext?.cartProducts.length ? (
+                        sampleCartContext?.cartProducts.map((p) => {
+                            return (
+                                <li className="item" key={p.id}>
+                                    <span className="item-image">
+                                        <img src={p.image} height="45rem" width="45rem" />{" "}
+                                    </span>
+                                    <div className="quntity-container">
+                                        <span
+                                            className="oparation"
+                                            onClick={() =>
+                                                sampleCartContext.decreaseCount(String(p.id))
+                                            }
+                                        >
+                                            -
+                                        </span>
+                                        <span>{p.count}</span>
+                                        <span
+                                            className="oparation"
+                                            onClick={() =>
+                                                sampleCartContext.increaseCount(String(p.id))
+                                            }
+                                        >
+                                            +
+                                        </span>
+                                    </div>
+                                    <span className="item-price">${(p.price * p.count).toFixed(2)}</span>
+                                    <i
+                                        className="fas fa-regular fa-trash"
+                                        onClick={() =>
+                                            sampleCartContext.deleteFromCart(String(p.id))
+                                        }
+                                    ></i>
+                                </li>
+                            );
+                        })
+                    ) : (
+                        <div className="no-product">
+                            <img src="./images/no-product1.jpg" />
+                        </div>
+                    )}
                 </ul>
             </div>
-            <div className='order'>
-                <p>total price</p>
-                {/* <p>total price</p> */}
+            <div className="order">
+                <form>
+                    <div className="orderField">
+                        <label>Sub Total</label>
+                        <input disabled value={subTotalPrice.toFixed(2)} />
+                    </div>
+                    <div className="orderField">
+                        <label>Tax (%)</label>
+                        <input type="number" value={input.tax} onChange={handleTaxChange} min='0' />
+                    </div>
+                    <div className="orderField">
+                        <label>Discount (%)</label>
+                        <input
+                            type="number"
+                            value={input.discount}
+                            onChange={handleDiscountChange}
+                            min='0'
+                        />
+                    </div>
+                    <div className="orderField last">
+                        <label>Total</label>
+                        <input disabled value={total.toFixed(2)} />
+                    </div>
+                </form>
                 <Button text='order' onClick={() => { }} />
             </div>
         </section>
